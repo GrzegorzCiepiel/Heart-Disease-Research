dc86fa86b54687e8e10e0a93a5fe4c8846896180
Grzegorz Ciepiel <grzegorz@ciepiel.pl>
2024-01-25 11:19:15 +0100

    first commit

---
+++ main.py
@@ -0,0 +1,15 @@
+
+import pandas as pd
+import numpy as np
+from scipy.stats import ttest_1samp
+
+# load data
+heart = pd.read_csv('heart_disease.csv')
+yes_hd = heart[heart.heart_disease == 'presence']
+no_hd = heart[heart.heart_disease == 'absence']
+
+chol_hd = yes_hd.chol
+print(np.mean(chol_hd))
+
+tstat, pval = ttest_1samp(chol_hd, 240)
+print(pval/2)
